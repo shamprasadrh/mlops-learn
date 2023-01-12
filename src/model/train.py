8d8f7175ee2dabf896ef1ed76470519963fe2aa3
ABHINAV <abhinav@ABHINAVs-MacBook-Pro.local>
2023-01-12 11:22:40 +0530

    Updated Code

--- src/model/train.py
+++ src/model/train.py
@@ -5,14 +5,15 @@ import glob
 import os
 
 import pandas as pd
+import mlflow
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    mlflow.sklearn.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -22,6 +23,11 @@ def main(args):
 
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
+    run_id = mlflow.last_active_run().info.run_id
+    print("Logged data and model in run {}".format(run_id))
+
+    
 
 
 def get_csvs_df(path):
@@ -33,7 +39,13 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+# Add function to split data
+def split_data(df):
+    """Splits data for taining and splitting
+    """
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=1)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
